83e6b5584795659d6e70be17267dfac85b8ef553
daniel-lee-user <elucidation2@gmail.com>
2019-01-26 15:24:16 -0800

    first commit

---
+++ src/main/cpp/OI.cpp
@@ -0,0 +1,26 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include "OI.h"
+#include <Commands/BucketMove.h>
+
+#include <WPILib.h>
+#include "ctre/Phoenix.h"
+
+OI::OI() : leftStick(new Joystick(1)), rightStick(new Joystick(0)) {
+  // Process operator interface input here.
+}
+
+Joystick* OI::getLeftStick()
+{
+  return leftStick;
+}
+
+Joystick* OI::getRightStick()
+{
+  return rightStick;
+}
